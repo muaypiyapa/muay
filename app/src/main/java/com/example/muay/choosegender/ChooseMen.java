650c78d61232e54fa008c6bf245fb5023ba8f532
Piyapa Issaranont <muay@Piyapas-MacBook-Pro-2.local>
2017-11-15 19:02:25 +0700

    first commit

---
+++ app/src/main/java/com/example/muay/choosegender/ChooseMen.java
@@ -0,0 +1,14 @@
+package com.example.muay.choosegender;
+
+import android.view.View;
+
+/**
+ * Created by muay on 11/15/2017 AD.
+ */
+
+public class ChooseMen implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+
+    }
+}
